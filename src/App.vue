bb3d7f23788cb7a128dff9d7692291e3cf36e2cd
Arthur Novaes <arthurnovaes55@gmail.com>
2024-11-19 10:28:02 -0300

    feat: add busca de dados

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,66 @@
-<script setup lang="ts">
-import { useRoute } from "vue-router";
-const route = useRoute();
-</script>
-
 <template>
-  <div id="avisos"><strong>Current route path:</strong> {{ route.params }}</div>
+  <div>
+    <h1>Verificação CAPTCHA</h1>
+    <form @submit.prevent="submitForm">
+      <div
+        class="h-captcha"
+        :data-sitekey="siteKey"
+        data-size="invisible"
+        data-callback="onSubmit"
+      ></div>
+
+      <br />
+      <button type="submit" id="avisos">Avisos</button>
+      <button type="submit" id="impugnacoes">Impugnações</button>
+      <button type="submit" id="esclarecimentos">Esclarecimentos</button>
+    </form>
+
+    <div id="response">
+      <pre>{{ response }}</pre>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      siteKey: "b8bbded1-9d04-4ace-9952-b67cde081a7b",
+      response: "",
+    };
+  },
+  mounted() {
+    const script = document.createElement("script");
+    script.src = "https://hcaptcha.com/1/api.js";
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+
+    // Define a callback global para o hCaptcha
+    window.onSubmit = this.onSubmit;
+  },
+
+  methods: {
+    submitForm(event) {
+      const button = event.submitter;
+      this.activeButtonId = button.id;
+      window.hcaptcha.execute();
+    },
+    onSubmit(token) {
+      const url = new URL(
+        `https://cnetmobile.estaleiro.serpro.gov.br/comprasnet-fase-externa/public/v1/compras/19311705000022023/quadro-informativo/${this.activeButtonId}`
+      );
+      url.searchParams.append("captcha", token);
+
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          this.response = JSON.stringify(data, null, 2);
+        })
+        .catch((error) => {
+          this.response = `Erro: ${error}`;
+        });
+    },
+  },
+};
+</script>
